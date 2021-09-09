1f10ba9ba40f7cf3a74c8bfccda33b4fc6b4ca04
krgarima <kumargarima59@gmail.com>
2021-09-09 13:45:19 +0530

    button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-console.log("script");
+var btnTranslate= document.querySelector("#btn-translate");
 
-var username = prompt("Give me your username..");
-var welcomeMessage = username+ " is your username";
-alert(welcomeMessage);
+function clickEventHandler() {
+    console.log("Clickeddd");}
+
+btnTranslate.addEventListener("click", clickEventHandler())
